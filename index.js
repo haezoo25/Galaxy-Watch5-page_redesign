e14f783c357e2472c29b12ead6d8d839d650f232
haezoo <haezoo25@gmail.com>
2023-03-06 17:44:24 +0900

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+
+    $(".scroll").click(function(event){
+    
+    event.preventDefault();
+    
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+    
+    });
+    
+    });
